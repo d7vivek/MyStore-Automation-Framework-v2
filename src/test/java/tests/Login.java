c5237b5b920573093893d97cd9b79bb6b425965f
d7vivek <dhavalevivek@gmail.com>
2024-10-18 23:08:52 +0530

    extent report modified

--- src/test/java/tests/Login.java
+++ src/test/java/tests/Login.java
@@ -26,108 +26,108 @@ import Reports.BaseTest;
 
 public class Login extends BaseTest { // Inherit from BaseTest
 
-    WebDriver driver = null;
-    Properties prop = new Properties();
-    HomePage homePage;
-    LoginPage loginPage;
-    MyAccountPage myAccountPage;
+	WebDriver driver = null;
+	Properties prop = new Properties();
+	HomePage homePage;
+	LoginPage loginPage;
+	MyAccountPage myAccountPage;
 
-    private static final String WARNING_MESSAGE = "Warning: No match for E-Mail Address and/or Password.";
+	private static final String WARNING_MESSAGE = "Warning: No match for E-Mail Address and/or Password.";
 
-    @BeforeMethod
-    public void setup() {
-        // Load properties file
-        File propFile = new File("src/test/java/properties/projectData.properties");
-        try (FileInputStream fis = new FileInputStream(propFile)) {
-            prop.load(fis);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+	@BeforeMethod
+	public void setup() {
+		// Load properties file
+		File propFile = new File("src/test/java/properties/projectData.properties");
+		try (FileInputStream fis = new FileInputStream(propFile)) {
+			prop.load(fis);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        String browserName = prop.getProperty("browser");
+		String browserName = prop.getProperty("browser");
 
-        // Browser initialization using switch statement
-        switch (browserName.toLowerCase()) {
-            case "chrome":
-                driver = new ChromeDriver();
-                break;
-            case "edge":
-                driver = new EdgeDriver();
-                break;
-            default:
-                throw new IllegalArgumentException("Browser not supported: " + browserName);
-        }
+		// Browser initialization using switch statement
+		switch (browserName.toLowerCase()) {
+		case "chrome":
+			driver = new ChromeDriver();
+			break;
+		case "edge":
+			driver = new EdgeDriver();
+			break;
+		default:
+			throw new IllegalArgumentException("Browser not supported: " + browserName);
+		}
 
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        driver.get(prop.getProperty("url"));
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get(prop.getProperty("url"));
 
-        homePage = new HomePage(driver);
-        loginPage = homePage.clickOnMyAccount().selectLoginOption();
-    }
+		homePage = new HomePage(driver);
+		loginPage = homePage.clickOnMyAccount().selectLoginOption();
+	}
 
-    @AfterMethod
-    public void tearDown(ITestResult result) {
-        // Log test results in Extent Reports
-        if (result.getStatus() == ITestResult.FAILURE) {
-            test.log(Status.FAIL, MarkupHelper.createLabel(result.getName() + " - Test Case Failed", ExtentColor.RED));
-            test.fail(result.getThrowable());
-        } else if (result.getStatus() == ITestResult.SUCCESS) {
-            test.log(Status.PASS, MarkupHelper.createLabel(result.getName() + " - Test Case Passed", ExtentColor.GREEN));
-        } else if (result.getStatus() == ITestResult.SKIP) {
-            test.log(Status.SKIP, MarkupHelper.createLabel(result.getName() + " - Test Case Skipped", ExtentColor.ORANGE));
-            test.skip(result.getThrowable());
-        }
+	@AfterMethod
+	public void tearDown(ITestResult result) {
+		// Log test results in Extent Reports
+		if (result.getStatus() == ITestResult.FAILURE) {
+			test.log(Status.FAIL, MarkupHelper.createLabel(result.getName() + " - Test Case Failed", ExtentColor.RED));
+			test.fail(result.getThrowable());
+		} else if (result.getStatus() == ITestResult.SUCCESS) {
+			test.log(Status.PASS, MarkupHelper.createLabel(result.getName() + " - Test Case Passed", ExtentColor.GREEN));
+		} else if (result.getStatus() == ITestResult.SKIP) {
+			test.log(Status.SKIP, MarkupHelper.createLabel(result.getName() + " - Test Case Skipped", ExtentColor.ORANGE));
+			test.skip(result.getThrowable());
+		}
 
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+		if (driver != null) {
+			driver.quit();
+		}
+	}
 
-    @Test(priority = 1)
-    public void loginWithValidCredentials() {
-        test = extent.createTest("Login with Valid Credentials"); // Create test in report
-        myAccountPage = loginPage.enterLoginEmailAddress(prop.getProperty("email"))
-                .enterLoginPassword(prop.getProperty("password"))
-                .clickOnLoginButton();
-        Assert.assertTrue(myAccountPage.LoggedInStatus());
-    }
+	@Test(priority = 1)
+	public void loginWithValidCredentials() {
+		test = extent.createTest("Login with Valid Credentials"); // Create test in report
+		myAccountPage = loginPage.enterLoginEmailAddress(prop.getProperty("email"))
+				.enterLoginPassword(prop.getProperty("password"))
+				.clickOnLoginButton();
+		Assert.assertTrue(myAccountPage.LoggedInStatus());
+	}
 
-    @Test(priority = 2)
-    public void loginWithInvalidUsername() {
-        test = extent.createTest("Login with Invalid Username");
-        loginPage.enterLoginEmailAddress(prop.getProperty("invalidemail"))
-                .enterLoginPassword(prop.getProperty("invalidpassword"))
-                .clickOnLoginButton();
-        String actualWarning = loginPage.retriveWarningMessage();
-        Assert.assertEquals(actualWarning, WARNING_MESSAGE, "There is a problem with the warning message");
-    }
+	@Test(priority = 2)
+	public void loginWithInvalidUsername() {
+		test = extent.createTest("Login with Invalid Username");
+		loginPage.enterLoginEmailAddress(prop.getProperty("invalidemail"))
+		.enterLoginPassword(prop.getProperty("invalidpassword"))
+		.clickOnLoginButton();
+		String actualWarning = loginPage.retriveWarningMessage();
+		Assert.assertEquals(actualWarning, WARNING_MESSAGE, "There is a problem with the warning message");
+	}
 
-    @Test(priority = 3)
-    public void loginWithInvalidPassword() {
-        test = extent.createTest("Login with Invalid Password");
-        loginPage.enterLoginEmailAddress(prop.getProperty("email"))
-                .enterLoginPassword(prop.getProperty("invalidpassword"))
-                .clickOnLoginButton();
-        String actualWarning = loginPage.retriveWarningMessage();
-        Assert.assertEquals(actualWarning, WARNING_MESSAGE, "There is a problem with the warning message");
-    }
+	@Test(priority = 3)
+	public void loginWithInvalidPassword() {
+		test = extent.createTest("Login with Invalid Password");
+		loginPage.enterLoginEmailAddress(prop.getProperty("email"))
+		.enterLoginPassword(prop.getProperty("invalidpassword"))
+		.clickOnLoginButton();
+		String actualWarning = loginPage.retriveWarningMessage();
+		Assert.assertEquals(actualWarning, WARNING_MESSAGE, "There is a problem with the warning message");
+	}
 
-    @Test(priority = 4)
-    public void loginWithInvalidUsernamePassword() {
-        test = extent.createTest("Login with Invalid Username and Password");
-        loginPage.enterLoginEmailAddress(prop.getProperty("invalidemail"))
-                .enterLoginPassword(prop.getProperty("invalidpassword"))
-                .clickOnLoginButton();
-        String actualWarning = loginPage.retriveWarningMessage();
-        Assert.assertEquals(actualWarning, WARNING_MESSAGE, "There is a problem with the warning message");
-    }
+	@Test(priority = 4)
+	public void loginWithInvalidUsernamePassword() {
+		test = extent.createTest("Login with Invalid Username and Password");
+		loginPage.enterLoginEmailAddress(prop.getProperty("invalidemail"))
+		.enterLoginPassword(prop.getProperty("invalidpassword"))
+		.clickOnLoginButton();
+		String actualWarning = loginPage.retriveWarningMessage();
+		Assert.assertEquals(actualWarning, WARNING_MESSAGE, "There is a problem with the warning message");
+	}
 
-    @Test(priority = 5)
-    public void loginWithBlankUsernamePassword() {
-        test = extent.createTest("Login with Blank Username and Password");
-        loginPage.clickOnLoginButton();
-        String actualWarning = loginPage.retriveWarningMessage();
-        Assert.assertEquals(actualWarning, WARNING_MESSAGE, "There is a problem with the warning message");
-    }
+	@Test(priority = 5)
+	public void loginWithBlankUsernamePassword() {
+		test = extent.createTest("Login with Blank Username and Password");
+		loginPage.clickOnLoginButton();
+		String actualWarning = loginPage.retriveWarningMessage();
+		Assert.assertEquals(actualWarning, WARNING_MESSAGE, "There is a problem with the warning message");
+	}
 }
--- src/test/java/tests/Register.java
+++ src/test/java/tests/Register.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.time.Duration;
 import java.util.Date;
 import java.util.Properties;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -14,125 +13,141 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
+import com.aventstack.extentreports.Status;
+import Reports.BaseTest;  // Import BaseTest which includes ExtentReport setup
 import pageobjects.AccountSuccessPage;
 import pageobjects.HomePage;
 import pageobjects.RegisterPage;
 
-public class Register {
+public class Register extends BaseTest {  // Extend from BaseTest to inherit ExtentReports
 
-	WebDriver driver = null;
-	Properties prop = null;
-	RegisterPage registerpage = null ;
-	AccountSuccessPage accountsuccesspage = null;
+    WebDriver driver = null;
+    Properties prop = null;
+    RegisterPage registerpage = null ;
+    AccountSuccessPage accountsuccesspage = null;
 
-	@BeforeMethod
-	public void setup() {
+    @BeforeMethod
+    public void setup() {
+        // Start a new test in the ExtentReport
+        test = extent.createTest("Register Test - Setup");
 
-		prop = new Properties();
-		try {
-			prop.load(new FileInputStream(new File("C:\\selenium-workspace\\practice-workspace\\javapractice\\pomPfProject\\src\\test\\java\\properties\\projectData.properties")));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        prop = new Properties();
+        try {
+            prop.load(new FileInputStream(new File("C:\\selenium-workspace\\practice-workspace\\javapractice\\pomPfProject\\src\\test\\java\\properties\\projectData.properties")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        String browserName = prop.getProperty("browser");
 
-		String browserName = prop.getProperty("browser");
+        if(browserName.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        } else if(browserName.equalsIgnoreCase("edge")) {
+            driver = new EdgeDriver();
+        }
 
-		if(browserName.equalsIgnoreCase("chrome")) {
-			driver = new ChromeDriver();
-		}else if(browserName.equalsIgnoreCase("edge")) {
-			driver = new EdgeDriver();
-		}
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get(prop.getProperty("url"));
 
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		driver.get(prop.getProperty("url"));
+        HomePage homepage = new HomePage(driver);
+        homepage.clickOnMyAccount();
+        registerpage = homepage.slectRegisterOption();
 
-		HomePage homepage = new HomePage(driver);
+        // Log browser setup completion
+        test.log(Status.INFO, "Browser setup and navigation to register page completed.");
+    }
 
-		homepage.clickOnMyAccount();
-		registerpage = homepage.slectRegisterOption();	//method chaining we used 
+    @AfterMethod
+    public void tearDown() {
+        if(driver != null) {
+            driver.quit();
+        }
 
+        // Log teardown steps
+        test.log(Status.INFO, "Browser closed.");
+    }
 
-	}
-	@AfterMethod
-	public void tearDown() {
-		if(driver!=null) {
-			driver.quit();
-		}
+    @Test(priority = 1)
+    public void registerWithAllfield() {
+        test = extent.createTest("Register with All Fields Test");  // New test in report
 
-	}
+        accountsuccesspage = registerpage.enterFirstNameToRegister(prop.getProperty("firstname"))
+                .enterLastNametoRegister(prop.getProperty("lastname"))
+                .enterEmailToRegister(genrateNewEmail())
+                .enterTeliphone(prop.getProperty("teliphone"))
+                .enterPassword(prop.getProperty("password"))
+                .enterCnfPassword(prop.getProperty("password"))
+                .clickOnYesNewsLetter()
+                .CheckOnPrivacyPolicy()
+                .clickOnSubmitButton();  // Log details of the registration process
 
-	@Test(priority = 1)
-	public void registerWithAllfield() {
+        // Validate the expected outcome
+        String expectedPageHeading = "Your Account Has Been Created!";
+        String actualPageHeading = accountsuccesspage.retriveAccountSucessPageHeading();
+        Assert.assertEquals(expectedPageHeading, actualPageHeading);
 
+        // Log the test result
+        test.log(Status.PASS, "Register with all fields completed successfully.");
+    }
 
-		accountsuccesspage =registerpage.enterFirstNameToRegister(prop.getProperty("firstname"))
-				.enterLastNametoRegister(prop.getProperty("lastname"))
-				.enterEmailToRegister(genrateNewEmail())
-				.enterTeliphone(prop.getProperty("teliphone"))
-				.enterPassword(prop.getProperty("password"))
-				.enterCnfPassword(prop.getProperty("password"))
-				.clickOnYesNewsLetter()
-				.CheckOnPrivacyPolicy()
-				.clickOnSubmitButton();	// you can write this in a single line also , for readability is written in this format
+    @Test(priority = 2)
+    public void registerWithMandatoryField() {
+        test = extent.createTest("Register with Mandatory Fields Test");
 
-		String expectedPageHeading = "Your Account Has Been Created!";
-		String actualPageHeading = accountsuccesspage.retriveAccountSucessPageHeading();
-		Assert.assertEquals(expectedPageHeading, actualPageHeading);
+        accountsuccesspage = registerpage.enterFirstNameToRegister(prop.getProperty("firstname"))
+                .enterLastNametoRegister(prop.getProperty("lastname"))
+                .enterEmailToRegister(genrateNewEmail())
+                .enterTeliphone(prop.getProperty("teliphone"))
+                .enterPassword(prop.getProperty("password"))
+                .enterCnfPassword(prop.getProperty("password"))
+                .clickOnYesNewsLetter()
+                .CheckOnPrivacyPolicy().clickOnSubmitButton();
 
-	}
-	@Test (priority = 2)
-	public void registerWithMandatoryField() {
+        String expectedPageHeading = "Your Account Has Been Created!";
+        String actualPageHeading = accountsuccesspage.retriveAccountSucessPageHeading();
+        Assert.assertEquals(actualPageHeading, expectedPageHeading);
 
-		accountsuccesspage =registerpage.enterFirstNameToRegister(prop.getProperty("firstname"))
-				.enterLastNametoRegister(prop.getProperty("lastname"))
-				.enterEmailToRegister(genrateNewEmail())
-				.enterTeliphone(prop.getProperty("teliphone"))
-				.enterPassword(prop.getProperty("password"))
-				.enterCnfPassword(prop.getProperty("password"))
-				.clickOnYesNewsLetter()
-				.CheckOnPrivacyPolicy().clickOnSubmitButton();
-		
-		String expectedPageHeading = "Your Account Has Been Created!";
-		String actualPageHeading = accountsuccesspage.retriveAccountSucessPageHeading();
-		Assert.assertEquals(expectedPageHeading, actualPageHeading);
-	}
-	@Test(priority = 3)
-	public void registerWithoutEnteringAnyDetails() {
+        // Log the result in the report
+        test.log(Status.PASS, "Register with mandatory fields completed successfully.");
+    }
 
-		registerpage.clickOnSubmitButton();
-		String expectedError = "First Name must be between 1 and 32 characters!";
-		String actualFirstNameError = registerpage.retrivalfirstNameError();
-		Assert.assertEquals(actualFirstNameError,expectedError);
+    @Test(priority = 3)
+    public void registerWithoutEnteringAnyDetails() {
+        test = extent.createTest("Register without Entering Any Details Test");
 
-		String expectedLastNameError = "Last Name must be between 1 and 32 characters!";
-		String actualLastNameError = registerpage.retriveLastNameError();
-		Assert.assertEquals(actualLastNameError,expectedLastNameError);
+        registerpage.clickOnSubmitButton();
 
-		String expectedEmailError = "E-Mail Address does not appear to be valid!";
-		String actualEmailError = registerpage.retriveEmailError();
-		Assert.assertEquals(actualEmailError,expectedEmailError);
+        String expectedError = "First Name must be between 1 and 32 characters!";
+        String actualFirstNameError = registerpage.retrivalfirstNameError();
+        Assert.assertEquals(actualFirstNameError, expectedError);
 
-		String expectedTeliphoneError = "Telephone must be between 3 and 32 characters!";
-		String actualTeliphoneError = registerpage.retriveTeliphoneError();
-		Assert.assertEquals(actualTeliphoneError,expectedTeliphoneError);
+        String expectedLastNameError = "Last Name must be between 1 and 32 characters!";
+        String actualLastNameError = registerpage.retriveLastNameError();
+        Assert.assertEquals(actualLastNameError, expectedLastNameError);
 
-		String expectedPasswordError = "Password must be between 4 and 20 characters!";
-		String actualPasswordError = registerpage.retrivePasswordError();
-		Assert.assertEquals(actualPasswordError, expectedPasswordError);
+        String expectedEmailError = "E-Mail Address does not appear to be valid!";
+        String actualEmailError = registerpage.retriveEmailError();
+        Assert.assertEquals(actualEmailError, expectedEmailError);
 
-		String expectedPolicyError = "Warning: You must agree to the Privacy Policy!";
-		String actualPolicyError = registerpage.retrivePrivacyPolicyError();
-		Assert.assertEquals(actualPolicyError, expectedPolicyError);
+        String expectedTeliphoneError = "Telephone must be between 3 and 32 characters!";
+        String actualTeliphoneError = registerpage.retriveTeliphoneError();
+        Assert.assertEquals(actualTeliphoneError, expectedTeliphoneError);
 
-	}
+        String expectedPasswordError = "Password must be between 4 and 20 characters!";
+        String actualPasswordError = registerpage.retrivePasswordError();
+        Assert.assertEquals(actualPasswordError, expectedPasswordError);
 
-	public String genrateNewEmail() {
-		Date date = new Date();
-		return "ram"+date.toString().replaceAll(" ", "_").replace(":", "_")+"@gmail.com";
+        String expectedPolicyError = "Warning: You must agree to the Privacy Policy!";
+        String actualPolicyError = registerpage.retrivePrivacyPolicyError();
+        Assert.assertEquals(actualPolicyError, expectedPolicyError);
 
-	}
+        // Log the result in the report
+        test.log(Status.PASS, "Validation of all mandatory fields passed successfully.");
+    }
 
+    public String genrateNewEmail() {
+        Date date = new Date();
+        return "ram" + date.toString().replaceAll(" ", "_").replace(":", "_") + "@gmail.com";
+    }
 }
